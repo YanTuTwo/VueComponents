7f1e30c4a1445f783441cd44bb58c2a7b8400893
junjiezhang <454550789@qq.com>
2021-05-14 15:15:49 +0800

    ✨ 新增隔代组件通信方式

--- src/mixins/emitter.js
+++ src/mixins/emitter.js
@@ -1,5 +1,17 @@
+// 祖孙组件通信
+function broadcast(componentName, eventName, params) {
+    this.$children.forEach(child => {
+        let name = child.$options.componentName;
+        if (name === componentName) {
+            child.$emit.apply(child, [eventName, params])
+        } else {
+            broadcast.apply(child, [componentName, eventName, params])
+        }
+    });
+}
 export default {
     methods: {
+        // 通过自定义的组件名判断
         dispatch(componentName, eventName, params) {
             var parent = this.$parent || this.$root;
             var name = parent.$options.componentName;
@@ -7,7 +19,7 @@ export default {
             while (parent && (!name || name !== componentName)) {
                 parent = parent.$parent;
 
-                if (parent) {
+                if (parent) {  
                     name = parent.$options.componentName
                 }
             }
@@ -17,6 +29,10 @@ export default {
                 parent.$emit.apply(parent, [eventName, params])
                 // parent.$emit(eventName, params)
             }
+        },
+
+        broadcast(componentName, eventName, params) {
+            broadcast.apply(this, [componentName, eventName, params])
         }
     }
 }
